c80452db8b2fcb7f4857e93043ff6abd44dfef18
xray20221 <98450229+xray20221@users.noreply.github.com>
2022-01-26 05:27:25 -0500

    finish app basic functions

---
+++ obj.js
@@ -0,0 +1,17 @@
+const obj = {
+  rooms: 0,
+  clean: false,
+  repair: false,
+  fence: 0,
+  insurance: 0,
+  price: 0,
+  bonus0: true,
+  bonus2: false,
+  bonus4: false,
+  bonus6: false,
+  ver: 'a-0.0.3'
+}
+
+const api = 'https://api.pancakeswap.info/api/v2/tokens/0xfa57ff4670f9016069e799a51a3186d03991e431'
+
+export { obj, api };
